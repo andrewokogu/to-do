a910201edde1641ab47c0e40350fd53009cbb3f1
Ejiro Andrew Okogu <ejirookogu@gmail.com>
2021-11-30 15:01:28 +0100

    db

--- to_do/settings.py
+++ to_do/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 env = os.getenv('ENVIRONMENT', 'development')
 
-print(env)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
